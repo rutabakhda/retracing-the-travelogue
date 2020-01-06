bcf03024423f6ccb749229d4f3b1713b8cdc2963
rutabakhda <connectruta@gmail.com>
2020-01-06 17:43:11 +0100

    update final dictionary

--- src/data-preparation/final-dictionary.py
+++ src/data-preparation/final-dictionary.py
@@ -1,8 +1,10 @@
 import os
+from pathlib import Path
 import pandas as pd
 
-basepath = os.path.dirname(os.path.abspath(__file__))
-datapath = basepath + '/data/'
+datapath = Path(__file__).resolve().parents[2]
+
+print(f'Current working directory: {datapath}')
 
 """
     This code combines 2 different versions of indexes.
@@ -30,10 +32,10 @@ def combine_lists(list1, list2):
 
     final_list = (unique_in_list1 + list2)
     final_list = sorted(final_list)
-    return final_list
+    return [final_list,common_words,unique_in_list1]
 
 
-def list_to_csv(list, dataframe1, dataframe2):
+def list_to_csv(list, dataframe1, dataframe2,common_words,unique_in_murray):
 
     """
     :param list: list that needs to be stored as csv
@@ -45,27 +47,39 @@ def list_to_csv(list, dataframe1, dataframe2):
     new_data = pd.DataFrame(columns=['Entity', 'Tag'])
     new_row = {}
     for entry in list:
-        temp = dataframe1[dataframe1['Entity Name'] == entry]
-        if temp.empty:
-            tag = " "
+
+        if entry in common_words:
+            reference = 'Murray,Yule'
+        elif entry in unique_in_murray:
+            reference = 'Murray'
         else:
-            tag = temp['Tag'].iloc[0]
+            reference = 'Yule'
+
+        temp1 = dataframe1[dataframe1['Entity Name'] == entry]
+        temp2 = dataframe2[dataframe2['Entity Name'] == entry]
+        if not temp1.empty:
+            tag = temp1['Tag'].iloc[0]
+        elif not temp2.empty:
+            tag = temp2['Tag'].iloc[0]
+        else:
+            tag = " "
         new_row["Entity"] = entry
         new_row['Tag'] = tag
+        new_row['Reference'] = reference
         new_data.loc[len(new_data)] = new_row
     new_data.to_csv(datapath + "final-list.csv", sep=',', encoding='latin1')
 
 
-readfile_murray = 'hugh-murray/index/index.csv'
-readfile_yule = 'henry-yule/index/Book1.txt'
+readfile_murray = datapath / 'data/hugh-murray/index/index.csv'
+readfile_yule = datapath / 'data/henry-yule/index/Book1.txt'
 
 # Reads csv of index and convert it into list with unique entities
-df_murray=pd.read_csv(datapath+readfile_murray,sep=',',encoding='latin1')
+df_murray=pd.read_csv(readfile_murray,sep=',',encoding='latin1')
 murray_list = df_murray['Entity Name'].unique().tolist()
 
-df_yule=pd.read_csv(datapath+readfile_yule,sep='\t',encoding='latin1')
+df_yule=pd.read_csv(readfile_yule,sep='\t',encoding='latin1')
 yule_list = df_yule['Entity Name'].unique().tolist()
 
-final_list = combine_lists(murray_list,yule_list)
-list_to_csv(final_list,df_murray,df_yule)
+final_list,common_words,unique_in_murray = combine_lists(murray_list,yule_list)
 
+list_to_csv(final_list,df_murray,df_yule,common_words,unique_in_murray)
