6d198ce7280901981598a9027d50870772064ecc
ruta bakhda <connectruta@gmail.com>
2020-01-29 19:59:40 +0100

    bigrams and trigrams for gazzetter

--- src/ner-taggers/gazzeter-ner.py
+++ src/ner-taggers/gazzeter-ner.py
@@ -14,17 +14,49 @@ def convert(string):
     return li
 
 
+def generate_ngrams(tokens, n):
+    # Use the zip function to help us generate n-grams
+    # Concatentate the tokens into ngrams and return
+    ngrams = zip(*[tokens[i:] for i in range(n)])
+    return [" ".join(ngram) for ngram in ngrams]
+
+def find_tag(df,series,common):
+    ner_person = []
+    ner_location = []
+
+    if common:
+        for entry in common:
+
+            temp1 = df.loc[series.str.contains(entry, na=False)]
+            tag = temp1['Tag'].iloc[0]
+
+            if tag == 'Person':
+                ner_person.append(entry)
+
+            elif tag == 'Location':
+              ner_location.append(entry)
+    return ner_person,ner_location
+
+
+
 datapath = Path(__file__).resolve().parents[2]
 
 # Input individual index files
-readfile_murray = datapath / 'data/hugh-murray/index/index.csv'
+readfile_murray = datapath / 'data/final-list.csv'
 df_murray = pd.read_csv(readfile_murray, sep=',', encoding='latin1')
 df_murray['Entity Name'] = df_murray['Entity Name'].apply(lambda s: s.strip())
 murray_list = df_murray['Entity Name'].unique().tolist()
-
 murray_list = [item.strip() for item in murray_list]
 
-readfile = datapath / 'data/hugh-murray/chapter3/chapter3.csv'
+murray_alternative_list = []
+
+for index,item in df_murray['Alternative Name'].iteritems():
+    if not isinstance(item,float):
+        murray_alternative_list = murray_alternative_list + item.split(",")
+
+murray_alternative_list = list(set(murray_alternative_list))
+
+readfile = datapath / 'data/hugh-murray/chapter1/chapter1-common.csv'
 data = pd.read_csv(readfile,sep=',', encoding='latin1',error_bad_lines=False)
 
 location_list = []
@@ -34,31 +66,42 @@ count = 0
 
 for index,row in data.iterrows():
     sentence = row['sentence']
-    #sentence = "Having mentioned this King Dor, I will tell you a curious story of what passed between him and Prester John."
+    #sentence = "The great port of Khamil had many tourists there."
     remove = string.punctuation
     remove = remove.replace("-", "")  # don't remove hyphens
     remove = remove.replace("'", "")  # don't remove hyphens
     pattern = r"[{}]".format(remove)
     sentence = re.sub(pattern," ",sentence)
-    #sentence = sentence.translate(str.maketrans(' ', ' ', string.punctuation))
     sentence_list = convert(sentence)
-    print(sentence_list)
-    #print(sentence_list)
     sentence_list = [item.strip() for item in sentence_list]
-    common = [value for value in sentence_list if value in murray_list]
-    print(common)
+
+    sentence_bigram = generate_ngrams(sentence_list,2)
+    sentence_trigram = generate_ngrams(sentence_list,3)
+    #print(sentence_bigram)
+
+    common_1gram = [value for value in sentence_list if value in murray_list]
+    common_alternative_1gram = [value for value in sentence_list if value in murray_alternative_list]
+
+    common_bigram = [value for value in sentence_bigram if value in murray_list]
+    common_alternative_bigram = [value for value in sentence_bigram if value in murray_alternative_list]
+
+    common_trigram = [value for value in sentence_trigram if value in murray_list]
+    common_alternative_trigram = [value for value in sentence_trigram if value in murray_alternative_list]
+
     ner_person = []
     ner_location = []
-    for entry in common:
-        print(entry)
-        temp1 = df_murray.loc[df_murray['Entity Name'] == entry]
-        tag = temp1['Tag'].iloc[0]
-        print(tag)
-        if tag == 'Person':
-            ner_person.append(entry)
 
-        elif tag == 'Location':
-          ner_location.append(entry)
+    [ner_person_main_1gram,ner_location_main_1gram] = find_tag(df_murray,df_murray['Entity Name'],common_1gram)
+    [ner_person_alternative_1gram,ner_location_alternative_1gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_1gram)
+
+    [ner_person_main_2gram,ner_location_main_2gram] = find_tag(df_murray,df_murray['Entity Name'],common_bigram)
+    [ner_person_alternative_2gram,ner_location_alternative_2gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_bigram)
+
+    [ner_person_main_3gram,ner_location_main_3gram] = find_tag(df_murray,df_murray['Entity Name'],common_trigram)
+    [ner_person_alternative_3gram,ner_location_alternative_3gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_trigram)
+
+    ner_person = ner_person_main_1gram + ner_person_main_2gram + ner_person_main_3gram + ner_person_alternative_1gram + ner_person_alternative_2gram + ner_person_alternative_3gram
+    ner_location = ner_location_main_1gram + ner_location_main_2gram + ner_location_main_3gram + ner_location_alternative_1gram + ner_location_alternative_2gram + ner_location_alternative_3gram
 
     location = ', '.join([str(elem) for elem in ner_location])
     person = ' ,'.join([str(elem) for elem in ner_person])
@@ -71,6 +114,6 @@ for index,row in data.iterrows():
 data['Gazzeter Location'] = location_list
 data['Gazzeter Person'] = person_list
 
-writefile = datapath / 'data/hugh-murray/chapter3/chapter3-gazzeter.csv'
+writefile = datapath / 'data/hugh-murray/chapter3/chapter1-common1.csv'
 
 data.to_csv(writefile, sep=',', encoding='latin1')
