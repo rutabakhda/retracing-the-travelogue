af50eccf4044f952d723d11af7313100238dfb0f
ruta bakhda <connectruta@gmail.com>
2020-02-04 20:01:44 +0100

    updating index

--- src/ner-taggers/gazzeter-ner.py
+++ src/ner-taggers/gazzeter-ner.py
@@ -20,16 +20,19 @@ def generate_ngrams(tokens, n):
     ngrams = zip(*[tokens[i:] for i in range(n)])
     return [" ".join(ngram) for ngram in ngrams]
 
-def find_tag(df,series,common):
+def find_tag(df,series,common,series_type):
     ner_person = []
     ner_location = []
 
     if common:
+
         for entry in common:
+            if series_type == "alternative":
+                temp1 = df.loc[series.str.contains(entry, na=False)]
+            elif series_type == "main":
+                temp1 = df.loc[series.isin([entry])]
 
-            temp1 = df.loc[series.str.contains(entry, na=False)]
             tag = temp1['Tag'].iloc[0]
-
             if tag == 'Person':
                 ner_person.append(entry)
 
@@ -42,7 +45,7 @@ def find_tag(df,series,common):
 datapath = Path(__file__).resolve().parents[2]
 
 # Input individual index files
-readfile_murray = datapath / 'data/final-list.csv'
+readfile_murray = datapath / 'data/final-list.txt.csv'
 df_murray = pd.read_csv(readfile_murray, sep=',', encoding='latin1')
 df_murray['Entity Name'] = df_murray['Entity Name'].apply(lambda s: s.strip())
 murray_list = df_murray['Entity Name'].unique().tolist()
@@ -54,9 +57,10 @@ for index,item in df_murray['Alternative Name'].iteritems():
     if not isinstance(item,float):
         murray_alternative_list = murray_alternative_list + item.split(",")
 
-murray_alternative_list = list(set(murray_alternative_list))
+murray_alternative_list = list(set(murray_alternative_list))#
+murray_alternative_list = [value.strip() for value in murray_alternative_list]
 
-readfile = datapath / 'data/hugh-murray/chapter1/chapter1-common.csv'
+readfile = datapath / 'data/hugh-murray/chapter2/chapter2.csv'
 data = pd.read_csv(readfile,sep=',', encoding='latin1',error_bad_lines=False)
 
 location_list = []
@@ -66,7 +70,7 @@ count = 0
 
 for index,row in data.iterrows():
     sentence = row['sentence']
-    #sentence = "The great port of Khamil had many tourists there."
+    #sentence = 'These Marco, when the festivals of their idols come round, go to his majesty and say, " Great sire, you know the feast of such an idol approaches, and are aware that he can cause bad weather and much mischief to your cattle and grain. We pray,therefore,that you will give us all the sheep with black heads,also incense, aloe-wood, and such and such other things."'
     remove = string.punctuation
     remove = remove.replace("-", "")  # don't remove hyphens
     remove = remove.replace("'", "")  # don't remove hyphens
@@ -77,7 +81,7 @@ for index,row in data.iterrows():
 
     sentence_bigram = generate_ngrams(sentence_list,2)
     sentence_trigram = generate_ngrams(sentence_list,3)
-    #print(sentence_bigram)
+    #print(sentence_trigram)
 
     common_1gram = [value for value in sentence_list if value in murray_list]
     common_alternative_1gram = [value for value in sentence_list if value in murray_alternative_list]
@@ -88,24 +92,42 @@ for index,row in data.iterrows():
     common_trigram = [value for value in sentence_trigram if value in murray_list]
     common_alternative_trigram = [value for value in sentence_trigram if value in murray_alternative_list]
 
+    #print(common_1gram)
+    #print(common_alternative_1gram)
+
+    for value in (common_alternative_1gram + common_1gram):
+        if any(value in s for s in (common_bigram + common_alternative_bigram)):
+            if value in common_1gram:
+                common_1gram.remove(value)
+            if value in common_alternative_1gram:
+                common_alternative_1gram.remove(value)
+
+    for value in (common_bigram + common_alternative_bigram):
+        if any(value in s for s in (common_trigram + common_alternative_trigram)):
+            if value in common_bigram:
+                common_bigram.remove(value)
+            if value in common_alternative_bigram:
+                common_alternative_bigram.remove(value)
+
+
     ner_person = []
     ner_location = []
 
-    [ner_person_main_1gram,ner_location_main_1gram] = find_tag(df_murray,df_murray['Entity Name'],common_1gram)
-    [ner_person_alternative_1gram,ner_location_alternative_1gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_1gram)
+    [ner_person_main_1gram,ner_location_main_1gram] = find_tag(df_murray,df_murray['Entity Name'],common_1gram,"main")
+    [ner_person_alternative_1gram,ner_location_alternative_1gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_1gram,"alternative")
+    #print(ner_person_main_1gram)
+    [ner_person_main_2gram,ner_location_main_2gram] = find_tag(df_murray,df_murray['Entity Name'],common_bigram,"main")
+    [ner_person_alternative_2gram,ner_location_alternative_2gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_bigram,"alternative")
 
-    [ner_person_main_2gram,ner_location_main_2gram] = find_tag(df_murray,df_murray['Entity Name'],common_bigram)
-    [ner_person_alternative_2gram,ner_location_alternative_2gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_bigram)
-
-    [ner_person_main_3gram,ner_location_main_3gram] = find_tag(df_murray,df_murray['Entity Name'],common_trigram)
-    [ner_person_alternative_3gram,ner_location_alternative_3gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_trigram)
+    [ner_person_main_3gram,ner_location_main_3gram] = find_tag(df_murray,df_murray['Entity Name'],common_trigram,"main")
+    [ner_person_alternative_3gram,ner_location_alternative_3gram] = find_tag(df_murray,df_murray['Alternative Name'], common_alternative_trigram,"alternative")
 
     ner_person = ner_person_main_1gram + ner_person_main_2gram + ner_person_main_3gram + ner_person_alternative_1gram + ner_person_alternative_2gram + ner_person_alternative_3gram
     ner_location = ner_location_main_1gram + ner_location_main_2gram + ner_location_main_3gram + ner_location_alternative_1gram + ner_location_alternative_2gram + ner_location_alternative_3gram
 
     location = ', '.join([str(elem) for elem in ner_location])
     person = ' ,'.join([str(elem) for elem in ner_person])
-
+    #print(person)
     location_list.append(location)
     person_list.append(person)
     count = count + 1
@@ -114,6 +136,6 @@ for index,row in data.iterrows():
 data['Gazzeter Location'] = location_list
 data['Gazzeter Person'] = person_list
 
-writefile = datapath / 'data/hugh-murray/chapter3/chapter1-common1.csv'
+writefile = datapath / 'data/hugh-murray/chapter2/chapter2-gazetter-combined.csv'
 
 data.to_csv(writefile, sep=',', encoding='latin1')
